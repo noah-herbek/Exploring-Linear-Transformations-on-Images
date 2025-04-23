a730c30cffd9c4dc31da517489d95a26d687f5f8
noah-herbek <noah.a.herbek@gmail.com>
2025-04-23 14:31:33 -0400

    Add files via upload

--- image_transform.py
+++ image_transform.py
@@ -32,13 +32,12 @@ def showimage(wantedimage):
     plt.show()
 
 
-
 # Setup
 image0 = Image.open("spencer.jpg")
 image0_np = np.array(image0)
 print(f'The dimension of the image_np is {image0_np.shape}')
 
-
+"""
 #Linear Transformation 1
 image1 = image_transform(image0, 1/2, 0, 0, 1/2)
 showimage(image1)
@@ -62,3 +61,7 @@ showimage(image1)
 #Linear Transformation 5
 image3 = image_transform(image3, 3/5, -4/5, -4/5, -3/5)
 showimage(image3)
+"""
+#Linear Transformation 6
+image4 = image_transform(image0, 0, 1, 0.5, 1)
+showimage(image4)
